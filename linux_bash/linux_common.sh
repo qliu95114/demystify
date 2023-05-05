175ced27fe868ab1d080651ded692e2244a1a981
Qing Liu <qliu95114@qq.com>
2023-05-05 21:26:24 +0800

    update linux common script, install apache and allow unlimited KeepAliveRequests

--- linux_bash/linux_common.sh
+++ linux_bash/linux_common.sh
@@ -5,10 +5,10 @@
 learningbash="This script config linux with basic software"
 echo $learningbash
 
-# installation
+# installation, when install tshark it will prompt diaglog for dumpcap setting, 
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install netfilter-persistent net-tools iptables tcpdump nano vim iputils-ping cron inetutils-traceroute iotop iftop iperf3 netcat apache2-utils moreutils tshark -y
+sudo apt install netfilter-persistent net-tools iptables tcpdump nano vim iputils-ping cron inetutils-traceroute iotop iftop iperf3 netcat apache2-utils moreutils apache2 -y
 
 # add powershell 
 sudo snap install powershell --classic  # LEGACY, but working in Ubuntu 22
@@ -17,11 +17,19 @@ sudo snap install powershell --classic  # LEGACY, but working in Ubuntu 22
 # dotnet tool install --global PowerShell 
 
 # install a web server
-sudo apt install apache2 -y
 sudo systemctl start apache2
 sudo systemctl enable apache2
 
+# MaxKeepAliveRequests: The maximum number of requests to allow
+# during a persistent connection. Set to 0 to allow an unlimited amount.
+sudo sed -i 's/MaxKeepAliveRequests 100/MaxKeepAliveRequests 0/g' /etc/apache2/apache2.conf
+
 # create a default webpage for apache2 and include the hostname
-sudo echo "<html><body><h1>$(hostname)</h1></body></html>" > /var/www/html/index.html
+sudo echo "<html><body><h1>$(hostname)</h1></body></html>" > /var/www/html/hostname.html
 
+# open tcpport 80 to allow web traffic
+sudo ufw allow 80/tcp
 
+# supress wireshark installation
+#echo "wireshark-common wireshark-common/install-setuid boolean true" | sudo debconf-set-selections
+#sudo DEBIAN_FRONTEND=noninteractive apt-get -y install wireshark tshark
