6c786f14344ce303541ac0fbadfb63d37a4f7801
Qing Liu <qliu95114@qq.com>
2023-06-12 21:25:09 +0800

    ws_server: readme Updated

--- connectivityscript/win/websocket/server/ws_server.ps1
+++ connectivityscript/win/websocket/server/ws_server.ps1
@@ -1,8 +1,11 @@
+# To use this program, ensure dotnet is installed on the machine 
+# invoke-webrequest "https://raw.githubusercontent.com/qliu95114/demystify/main/connectivityscript/win/websocket/server/ws_server.ps1" -OutFile "$env:temp\ws_server.ps1"; iex "$($env:temp)\ws_server.ps1"
+
 # create dotnet project
 mkdir C:\ws_server
 Set-Location C:\ws_server
 dotnet new web
-Set-Location c:\ws_server\program.cs
+delete c:\ws_server\program.cs
 
 # download program.cs from github
 (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/qliu95114/demystify/main/connectivityscript/win/websocket/server/Program.cs", "C:\ws_server\program.cs")
