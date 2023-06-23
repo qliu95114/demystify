02327876a812fab7ea34a3d0691430f9594e5ca3
Qing Liu <qliu95114@qq.com>
2023-06-23 11:27:31 +0800

    Add HostName /ContainerId/InstanceId

--- connectivityscript/win/get-containerid.ps1
+++ connectivityscript/win/get-containerid.ps1
@@ -16,4 +16,11 @@
 # author : qliu
 # time : 2023-06-23
 
-([xml](c:\windows\system32\curl "http://168.63.129.16/machine?comp=goalstate" -H "x-ms-guest-agent-name: WaAgent-2.7.0.0 (2.7.0.0)" -H "x-ms-version: 2012-11-30" -A """")).GoalState.Container| Format-list
+$containerid=([xml](c:\windows\system32\curl "http://168.63.129.16/machine?comp=goalstate" -H "x-ms-guest-agent-name: WaAgent-2.7.0.0 (2.7.0.0)" -H "x-ms-version: 2012-11-30" -A """")).GoalState.Container.ContainerId
+$InstanceId=([xml](c:\windows\system32\curl "http://168.63.129.16/machine?comp=goalstate" -H "x-ms-guest-agent-name: WaAgent-2.7.0.0 (2.7.0.0)" -H "x-ms-version: 2012-11-30" -A """")).GoalState.RoleInstanceList.RoleInstance.InstanceId
+
+$logdate = ((get-date).ToUniversalTime()).ToString("yyyy-MM-dd HH:mm:ss")
+
+Write-Host "[$($logdate)],HostName: $env:computername" -ForegroundColor Cyan
+Write-Host "[$($logdate)],ContainerId: $containerid" -ForegroundColor Cyan
+Write-Host "[$($logdate)],InstanceId: $InstanceId" -ForegroundColor Cyan
