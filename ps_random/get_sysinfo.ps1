3ee7015175eca601604248a1595c687c6ef4f680
Qing Liu <qliu95114@qq.com>
2025-07-27 10:10:05 +0800

    Enhance get_sysinfo.ps1 to accept ComputerName parameter for remote system information retrieval

--- ps_random/get_sysinfo.ps1
+++ ps_random/get_sysinfo.ps1
@@ -1,14 +1,39 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$ComputerName
+)
+
+# Test if the remote computer is accessible
+if (-not (Test-Connection -ComputerName $ComputerName -Quiet -Count 1)) {
+    Write-Error "Cannot connect to remote computer: $ComputerName"
+    return
+}
+
+# collect system model and service tag information and asset number
+$systemInfo = Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName $ComputerName | 
+    Select-Object Model, Manufacturer 
+
 # Collect CPU type information
-$cpuInfo = Get-CimInstance -ClassName Win32_Processor | Select-Object Name, Manufacturer, NumberOfCores, NumberOfLogicalProcessors
+$cpuInfo = Get-CimInstance -ClassName Win32_Processor -ComputerName $ComputerName | 
+    Select-Object Name, Manufacturer, NumberOfCores, NumberOfLogicalProcessors
 
 # Collect Memory size information (in GB)
-$memoryInfo = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object @{Name='TotalPhysicalMemory(GB)'; Expression={"{0:N2}" -f ($_.TotalPhysicalMemory / 1GB)}}
+$memoryInfo = Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName $ComputerName | 
+    Select-Object @{Name='TotalPhysicalMemory(GB)'; Expression={"{0:N2}" -f ($_.TotalPhysicalMemory / 1GB)}}
 
 # Collect Disk type and size information
-$diskInfo = Get-CimInstance -ClassName Win32_DiskDrive | Select-Object Model, MediaType, @{Name='Size(GB)'; Expression={"{0:N2}" -f ($_.Size / 1GB)}}
+$diskInfo = Get-CimInstance -ClassName Win32_DiskDrive -ComputerName $ComputerName | 
+    Select-Object Model, MediaType, @{Name='Size(GB)'; Expression={"{0:N2}" -f ($_.Size / 1GB)}} | Sort-Object Model
 
 # Output the collected information
-Write-Host "CPU Information:"
+Write-Host "System Information for computer: $ComputerName"
+Write-Host "-----------------------------------------"
+
+Write-Host "`nSystem Information:"
+$systemInfo | Format-Table -AutoSize
+
+Write-Host "`nCPU Information:"
 $cpuInfo | Format-Table -AutoSize
 
 Write-Host "`nMemory Information:"
