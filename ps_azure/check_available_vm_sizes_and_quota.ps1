b78be0db12c1242e2673e4e589333e425e87c151
Qing Liu <qliu95114@qq.com>
2025-11-25 13:04:52 +0800

    add -location only check logic

--- ps_azure/check_available_vm_sizes_and_quota.ps1
+++ ps_azure/check_available_vm_sizes_and_quota.ps1
@@ -9,10 +9,11 @@ param(
 # if subscriptionId is null, check current context and ask user if this is the subid we and to check if user provide a new subid, switch to that subid
 if ($subscriptionId) {
     Write-Host "Switching to Subscription ID: $subscriptionId"
-    Set-AzContext -SubscriptionId $subscriptionId
+    Connect-AzAccount -SubscriptionId $subscriptionId
+
 } else {
     $currentContext = Get-AzContext 
-    Write-Host "Current Subscription ID: $($currentContext.Subscription.Id)"
+    Write-Host "Current Subscription ID: $($currentContext.Subscription.Id) , Name: $($currentContext.Subscription.Name)"
     $userInput = Read-Host "Do you want to use this subscription? (Y/N)"
     if ($userInput -ne 'Y' -and $userInput -ne 'y') {
         $newSubId = Read-Host "Please enter the Subscription ID you want to switch to"
@@ -23,16 +24,18 @@ if ($subscriptionId) {
 # Get the current subscription context
 $currentContext = Get-AzContext 
 $currentSubscriptionId = $currentContext.Subscription.Id
-Write-Host "Using Subscription ID: $currentSubscriptionId"
+Write-Host "Using Subscription ID: $currentSubscriptionId" -ForegroundColor Cyan
+Write-Host "Using Subscription Name: $($currentContext.Subscription.Name)" -ForegroundColor Cyan
 
 # if location is not provided, use all location
 if (-not $location) {
     $locations = (Get-AzLocation).Location
 } else {
     $locations = @($location)
+    Write-Host "Checking only location: $location"  -ForegroundColor Cyan
 }
 
-if ($showall)    {
+if ($showall -or $location)    {
 }
 else {
     Write-host "-showall not specified, only locations that support the VM size will be shown." -ForegroundColor Yellow
@@ -40,7 +43,14 @@ else {
 
 foreach ($loc in $locations) {
    # Check the quota for the specified VM size
-   $quota = Get-AzComputeResourceSku -Location $loc |Where-Object {  $_.ResourceType -eq "virtualMachines"}| Where-Object { $_.name -like "*$($vmsize)*" }
+   if (-not $vmsize) {
+        $quota = Get-AzComputeResourceSku -Location $loc |Where-Object {  $_.ResourceType -eq "virtualMachines"} 
+        $quota.count
+   }
+   else {
+        $quota = Get-AzComputeResourceSku -Location $loc |Where-Object {  $_.ResourceType -eq "virtualMachines"}| Where-Object { $_.name -like "*$($vmsize)*" } 
+   }
+   
 
    # if no quota found, write message and continue to next location
    if (-not $quota) {
@@ -52,7 +62,7 @@ foreach ($loc in $locations) {
         # if $q.restrictions is empty, then no restrictions
         if (-not $q.Restrictions) {
 
-            Write-Host "Checking VM size '$vmsize' availability and quota in location '$loc'..." -ForegroundColor Cyan            
+            Write-Host "Checking VM size $($q.name) availability and quota in location '$loc'..." -ForegroundColor Cyan            
             # calculate and vm family name based on $q.name and verify quota for that vm family
             $i=$q.name.split('_').count
             $vmfamily = ""
@@ -98,7 +108,7 @@ foreach ($loc in $locations) {
         }
         else {
             # if restrictions found, write them 
-            if ($showall)   {
+            if ($showall -or (-not $vmsize))   {
                 Write-Host "'$loc' : support VM Size: $($q.Name), Restrictions: $($q.RestrictionInfo)" -ForegroundColor "Yellow"
                 foreach ($restriction in $q.Restrictions) {
                     Write-Host "    Type: $($restriction.Type), Reason Code: $($restriction.ReasonCode)" -ForegroundColor "Red"
